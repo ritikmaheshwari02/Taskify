e1a32f323bb1399f968f1b573f6422f3abd9ca13
Ritik Maheshwari <ritikisnamaheshwari2002@gmail.com>
2026-05-03 22:40:53 +0530

    Changed the DB to local

--- backend/src/main/java/com/taskify/entities/User.java
+++ backend/src/main/java/com/taskify/entities/User.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Data
 @Entity
 @Builder
+@Table(name = "Users")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
