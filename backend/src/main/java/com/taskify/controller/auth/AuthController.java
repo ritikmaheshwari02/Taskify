9ad0c5fa73df1a23f6b7ab669f2f2d10a065a0c8
Ritik Maheshwari <ritikisnamaheshwari2002@gmail.com>
2026-05-17 22:45:20 +0530

    login added to AuthController affected files AuthController, Authentication(req and res)

--- backend/src/main/java/com/taskify/controller/auth/AuthController.java
+++ backend/src/main/java/com/taskify/controller/auth/AuthController.java
@@ -7,7 +7,6 @@ import com.taskify.dto.UserDto;
 import com.taskify.entities.User;
 import com.taskify.repository.UserRepo;
 import com.taskify.services.auth.AuthService;
-import com.taskify.services.jwt.UserService;
 import com.taskify.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,13 +14,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -30,7 +27,7 @@ public class AuthController {
 
     private final AuthService authService;
     private final AuthenticationManager authenticationManager;
-    private final UserService userService;
+    //    private final UserService userService;
     private final UserRepo userRepo;
     private final JwtUtil jwtUtil;
 
@@ -59,16 +56,17 @@ public class AuthController {
         } catch (BadCredentialsException e) {
             throw new BadCredentialsException("Incorrect username or password");
         }
-        final UserDetails userDetails = userService.userDetailsService()
-                                                   .loadUserByUsername(authenticationRequest.getEmail());
-        Optional<User> optionalUser = userRepo.findFirstByEmail(authenticationRequest.getEmail());
-        final String jwt = jwtUtil.generateToken(userDetails);
+//        final UserDetails userDetails = userService.userDetailsService()
+//                                                   .loadUserByUsername(authenticationRequest.getEmail());
+        User user = userRepo.findFirstByEmail(authenticationRequest.getEmail())
+                            .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        final String jwt = jwtUtil.generateToken(user);
         AuthenticationResponse authenticationResponse = new AuthenticationResponse();
-        if (optionalUser.isPresent()) {
-            authenticationResponse.setJwt(jwt);
-            authenticationResponse.setUserId(optionalUser.get().getId());
-            authenticationResponse.setUserRole(optionalUser.get().getUserRole());
-        }
+
+        authenticationResponse.setJwt(jwt);
+        authenticationResponse.setUserId(user.getId());
+        authenticationResponse.setUserRole(user.getUserRole());
+
         return authenticationResponse;
     }
 }
